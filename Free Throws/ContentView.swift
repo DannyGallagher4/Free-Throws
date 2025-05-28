d462415b8648d16b9e809d06aaaee0ae31880e46
Danny Gallagher <ninjagallagher4@gmail.com>
2025-05-27 22:36:32 -0400

    firebase

--- Free Throws/ContentView.swift
+++ Free Throws/ContentView.swift
@@ -11,6 +11,8 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text("Free Throws")
+                .font(.title)
+            Spacer()
         }
         .padding()
     }
