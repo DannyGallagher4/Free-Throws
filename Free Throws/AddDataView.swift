1d707c637def0a112e2b335d416977ee69537d6e
Danny Gallagher <ninjagallagher4@gmail.com>
2025-05-27 23:48:33 -0400

    views

---
+++ Free Throws/AddDataView.swift
@@ -0,0 +1,18 @@
+//
+//  AddDataView.swift
+//  Free Throws
+//
+//  Created by Danny Gallagher on 5/27/25.
+//
+
+import SwiftUI
+
+struct AddDataView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddDataView()
+}
---
+++ Free Throws/ViewDataView.swift
@@ -0,0 +1,18 @@
+//
+//  ViewDataView.swift
+//  Free Throws
+//
+//  Created by Danny Gallagher on 5/27/25.
+//
+
+import SwiftUI
+
+struct ViewDataView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ViewDataView()
+}
